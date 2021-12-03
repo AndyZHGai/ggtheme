13faf3d8b3c6fddebe5f9186a24166ae4bb811bf
ubuntu <ubuntu@localhost.localdomain>
2021-12-03 11:37:44 +0800

    version 0.0.1

--- R/theme_pub.R
+++ R/theme_pub.R
@@ -16,7 +16,7 @@ theme_pub <- function(base_size = 14, base_family = "sans") {
   ggthemes::theme_foundation(base_size = base_size, base_family=base_family) +
     theme(plot.title = element_text(face = "bold", size = rel(1.2), hjust = 0.5,
                                     margin = margin(0,0,20,0)),
-            text = element_text(),
+            text = element_text(face = "bold"),
             panel.background = element_rect(colour = NA),
             plot.background = element_rect(colour = NA),
             panel.border = element_rect(colour = NA),
@@ -35,9 +35,11 @@ theme_pub <- function(base_size = 14, base_family = "sans") {
             legend.box = "vetical",
             legend.key.size= unit(0.5, "cm"),
             legend.margin = margin(0, 0, 0, 0),
-            legend.title = element_text(face = "italic"),
+            legend.title = element_blank(),
             plot.margin=unit(c(10, 5, 5, 5), "mm"),
             strip.background=element_rect(colour = "#f0f0f0", fill = "#f0f0f0"),
-            strip.text = element_text(face = "bold")
+            strip.text = element_text(face = "bold"),
+          plot.tag =           element_text(size = 25, family = "serif",
+                                            hjust = 0, vjust = -1)
     )
 }
